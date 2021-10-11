67761cb33707f30400151c197bb2addbdeceeb7e
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-11 13:33:42 -0700

    added cookbooks array for products

---
+++ cookbooks.js
@@ -0,0 +1,26 @@
+export const cookbooks = [
+    {
+        id: '1', 
+        name: 'Baking with Fortitude', 
+        img: './assets/fortitude.jpeg', 
+        publisher: 'Bloomsbury', 
+        cuisine: 'sweet', 
+        price: 28.00, 
+    }, 
+    {
+        id: '2', 
+        name: 'Jubilee', 
+        img: './assets/jubilee.jpeg',
+        publisher: 'Clarkson Potter', 
+        cuisine: 'savory', 
+        price: 35.00, 
+    }, 
+    {
+        id: '3', 
+        name: 'How to Taste',
+        img: './assets/taste',
+        publisher: 'Sasquatch', 
+        cuisine: 'savory', 
+        price: 22.95, 
+    }, 
+]
