453b163e93ebbf935be1bdfb79cd95e5a492816d
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-11 17:32:09 -0700

    updated TDD qunit testing

--- test/example.test.js
+++ test/example.test.js
@@ -8,7 +8,7 @@ const test = QUnit.test;
 test('renderBook should return an HTML snippet', (expect) => {
   
     // grab outerHTML from first object 
-    const expected = '<div class="product-card"><h3>Baking with Fortitude</h3><img src="./assets/fortitude.jpeg"><p>Dee Rettali</p><p>Bloomsbury</p><p>28</p></div>';
+    const expected = '<div class="product-card"><h3>Baking with Fortitude</h3><img src="./assets/fortitude.jpeg"><p class="author">Author: Dee Rettali</p><p class="publisher">Publisher: Bloomsbury</p><p class="price">Price: $28</p><button class="add-button">Add to cart</button></div>';
     
     // declare variable to grab first item from object
     const fortitude = cookbooks[0]; 
