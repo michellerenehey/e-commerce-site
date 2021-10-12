ddad4e193ca58cbb604f6be6b3e312cd50202779
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-12 13:26:06 -0700

    rendered table from cart.js file

--- cart/cart.js
+++ cart/cart.js
@@ -4,12 +4,32 @@
 // we'll use findById for that! 
 // finally, create the table using/building DOM elements
 
+// <<tr>
+//     <td>Baking with Fortitude</td>
+//     <td>28.00</td>
+//     <td>1</td>
+//     <td>28.00</td>
+// </tr>
+
 import { cookbooks } from '../data/cookbooks.js'; 
 import { cart } from '../data/cart-data.js'; 
 import { findById } from '../utils.js'; 
 
+// grab elements from HTML
+const tbody = document.getElementById('table-body'); 
+
 for (let cartItem of cart) {
     const bookData = findById(cartItem.id, cookbooks); 
-    
-    //next, create the table!
+    const tr = document.createElement('tr'); 
+    const tdName = document.createElement('td'); 
+    tdName.textContent = bookData.name; 
+    const tdPrice = document.createElement('td'); 
+    tdPrice.textContent = bookData.price;
+    const tdQty = document.createElement('td'); 
+    tdQty.textContent = cartItem.qty; 
+    const tdTotal = document.createElement('td'); 
+    tdTotal.textContent = cartItem.qty * bookData.price; 
+
+    tr.append(tdName, tdPrice, tdQty, tdTotal); 
+    tbody.append(tr);
 }
